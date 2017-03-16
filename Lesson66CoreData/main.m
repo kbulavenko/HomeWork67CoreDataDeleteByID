cd8770e1ee2599cdcfddb555b7c7bf26dd4a9eb0
Константин В. Булавенко <09197@mac-mini-7.local>
2017-03-16 20:20:27 +0200

    Initial Commit

---
+++ Lesson66CoreData/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson66CoreData
+//
+//  Created by Константин В. Булавенко on 16.03.17.
+//  Copyright © 2017 Константин В. Булавенко. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
